6077adc8b722cb2024a0fd1df5f2d208408d7b12
Alan Orther <alan.orther@vervemobile.com>
2020-04-29 12:35:31 -0700

    Update revoke.pp
    Had issues with the module from the commit that we pulled from so I forked.
    mod 'puppet-openvpn',
      :git => 'https://github.com/voxpupuli/puppet-openvpn',
      :commit => '827b3d6ff5b1311da5922bf36892d04d6c411f56'

--- manifests/revoke.pp
+++ manifests/revoke.pp
@@ -27,12 +27,12 @@ define openvpn::revoke (
 
   $revocation_command = $openvpn::easyrsa_version ? {
     '2.0' => ". ./vars && ./revoke-full ${name}; echo \"exit $?\" | grep -qE '(error 23|exit (0|2))' && touch revoked/${name}",
-    '3.0' => ". ./vars && ./easyrsa revoke --batch ${name}; echo \"exit $?\" | grep -qE '(error 23|exit (0|2))' && touch revoked/${name}",
+    '3.0' => ". ./vars && yes yes | ./easyrsa revoke ${name}; echo \"exit $?\" | grep -qE '(error 23|exit (0|2))' && touch revoked/${name}",
   }
 
   $renew_command = $openvpn::easyrsa_version ? {
-    '2.0'   => ". ./vars && KEY_CN='' KEY_OU='' KEY_NAME='' KEY_ALTNAMES='' openssl ca -gencrl -out ${openvpn::server_directory}/${name}/crl.pem -config ${openvpn::server_directory}/${name}/easy-rsa/openssl.cnf",
-    '3.0'   => ". ./vars && EASYRSA_REQ_CN='' EASYRSA_REQ_OU='' openssl ca -gencrl -out ${server_directory}/${name}/crl.pem -config ${server_directory}/${name}/easy-rsa/openssl.cnf",
+    '2.0'   => ". ./vars && KEY_CN='' KEY_OU='' KEY_NAME='' KEY_ALTNAMES='' openssl ca -gencrl -out ${openvpn::server_directory}/${server}/easy-rsa/keys/crl.pem -config ${openvpn::server_directory}/${server}/easy-rsa/openssl.cnf",
+    '3.0'   => ". ./vars && EASYRSA_REQ_CN='' EASYRSA_REQ_OU='' openssl ca -gencrl -out ${server_directory}/${server}/easy-rsa/keys/crl.pem -config ${server_directory}/${server}/easy-rsa/openssl.cnf",
     default => fail("unexepected value for EasyRSA version, got '${openvpn::easyrsa_version}', expect 2.0 or 3.0."),
   }
 
@@ -41,13 +41,13 @@ define openvpn::revoke (
     cwd      => "${server_directory}/${server}/easy-rsa",
     creates  => "${server_directory}/${server}/easy-rsa/revoked/${name}",
     provider => 'shell',
-    notify   => Exec["renew crl.pem on ${name}"],
+    notify   => Exec["renew crl.pem on ${server}"],
   }
 
-  exec { "renew crl.pem on ${name}":
+  exec { "renew crl.pem on ${server}":
     command  => $renew_command,
-    cwd      => "${openvpn::server_directory}/${name}/easy-rsa",
+    cwd      => "${openvpn::server_directory}/${server}/easy-rsa",
     provider => 'shell',
-    schedule => "renew crl.pem schedule on ${name}",
+    #schedule => "renew crl.pem schedule on ${server}",
   }
 }
